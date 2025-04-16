3f62dc620ae0bbfac006ce57a3fdf189f97e580e
luizscalfi <luizcarlostheodoro@hotmail.com>
2025-04-16 07:42:13 -0300

    mais um committ4  - projeto de rifas

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
 from config import Config
-from database import db
+from database import db  # usa a instância do db criada em database.py
 from routes import bp as api_bp
-from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+
+# Configurações
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+
+# Inicializa extensões
+db.init_app(app)
 CORS(app)
 
-db.init_app(app)
+# Registra blueprints
 app.register_blueprint(api_bp, url_prefix='/api')
 
+# Cria as tabelas no banco de dados
 with app.app_context():
     db.create_all()
 
