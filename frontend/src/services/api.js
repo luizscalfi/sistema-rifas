f1505252a1530226e76db6ecd6a4f9e2245b5d34
luizscalfi <luizcarlostheodoro@hotmail.com>
2025-04-16 09:24:43 -0300

    mais um committ6  - projeto de rifas

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sistema-rifas.onrender.com'
+  baseURL: 'https://sistema-rifas.onrender.com/api'
 });
 
 export default api;
