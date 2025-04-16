9cacd8877bc7376d58aff28ca7225a45ab7e3008
luizscalfi <luizcarlostheodoro@hotmail.com>
2025-04-16 07:57:22 -0300

    mais um committ5  - projeto de rifas

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
 from config import Config
-from database import db  # usa a instância do db criada em database.py
+from database import db
 from routes import bp as api_bp
-import os
 
 app = Flask(__name__)
-
-# Configurações
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config.from_object(Config)
 
-# Inicializa extensões
 db.init_app(app)
 CORS(app)
 
-# Registra blueprints
 app.register_blueprint(api_bp, url_prefix='/api')
 
-# Cria as tabelas no banco de dados
 with app.app_context():
     db.create_all()
 
--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/rifas_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')  # <- pega da env
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
