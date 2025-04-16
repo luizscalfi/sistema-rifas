255f7027415950df29fe765d82341493c9030dcb
luizscalfi <luizcarlostheodoro@hotmail.com>
2025-04-16 00:37:38 -0300

    mais um committ - projeto de rifas

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,12 @@ from flask_cors import CORS
 from config import Config
 from database import db
 from routes import bp as api_bp
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config.from_object(Config)
+db = SQLAlchemy(app)
 CORS(app)
 
 db.init_app(app)
